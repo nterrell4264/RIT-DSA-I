fbfa9542c49b84e7664ee1dd3758006403c96098
nat7611 <nat7611@g.rit.edu>
2018-02-14 18:00:21 -0500

    PE8

---
+++ PE8 - ToePoint/PE8 - ToePoint/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void changeVariable(double doub);
+void changePointer(double* doub);
+
+int getLengthArray(int data[]);
+int getLengthPointer(int* pointer);
+
+int* createStackArray();
+int* createHeapArray(int size);
