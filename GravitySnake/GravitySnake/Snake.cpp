0bc1fa2a23b9d29e9f342efea7396d0f6e94e886
Nathan Terrell <nat7611@rit.edu>
2018-02-25 13:55:58 -0500

    Start of HW2

---
+++ GravitySnake/GravitySnake/Snake.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "conio.h"
+#include "snake.h"
+using namespace std;
+
+float* position[2] = {};
+float* velocity[2] = {};
+float* acceleration[2] = {};
+
+void update() {
+	applyForces();
+	
+}
+
+void applyForces() {
+
+}
+void moveTarget(float& xPos, float& yPos) {
+
+}
+
+void display() {
+
+}
