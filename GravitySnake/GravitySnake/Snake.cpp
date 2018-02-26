47ceb1ba2718091fe7c97549aa1e05680b68962a
nat7611 <nat7611@g.rit.edu>
2018-02-25 23:25:59 -0500

    Part 1, except it has linker errors because I'm creating global variables.

--- GravitySnake/GravitySnake/Snake.cpp
+++ GravitySnake/GravitySnake/Snake.cpp
@@ -1,24 +1,47 @@
+#pragma region dependencies
 #include "stdafx.h"
 #include "conio.h"
 #include "snake.h"
-using namespace std;
-
-float* position[2] = {};
-float* velocity[2] = {};
-float* acceleration[2] = {};
+//SFML
+#define SFML_STATIC
+#include <SFML\Window.hpp>
+#include <SFML\Graphics.hpp>
+#pragma endregion
 
 void update() {
 	applyForces();
-	
+	world.Step(timeStep, velocityIterations, positionIterations);
+	display();
 }
 
 void applyForces() {
-
+	if (_kbhit != 0) {
+		switch (_getch()) {
+		case('W'):
+		case(38): {
+			snake->ApplyForceToCenter(b2Vec2(0.0f, 1.0f),true);
+			break;
+		}
+		case('A'):
+		case(37): {
+			snake->ApplyForceToCenter(b2Vec2(-1.0f, 0.0f), true);
+			break;
+		}
+		case('D'):
+		case(39): {
+			snake->ApplyForceToCenter(b2Vec2(1.0f, 0.0f), true);
+			break;
+		}
+		}
+	}
 }
 void moveTarget(float& xPos, float& yPos) {
-
+	target->SetTransform(b2Vec2(xPos, yPos), 0);
 }
 
 void display() {
-
+	b2Vec2 prPos = snake->GetPosition();
+	printf("Snake is at %f, %f\n", prPos.x, prPos.y);
+	prPos = target->GetPosition();
+	printf("Target is at %f, %f\n", prPos.x, prPos.y);
 }
--- GravitySnake/GravitySnake/snake.h
+++ GravitySnake/GravitySnake/snake.h
@@ -1,5 +1,18 @@
 #pragma once
+//Includes
+#include "Box2D/box2d.h"
 
+//Variables
+b2Vec2 gravity(0.0f, -9.8f);
+b2World world = b2World(gravity);
+b2Body* snake;
+b2Body* target;
+
+float32 timeStep = 1 / 60.0f;
+int32 velocityIterations = 6;
+int32 positionIterations = 2;
+
+//Functions
 void update();
 void display();
 void applyForces();
