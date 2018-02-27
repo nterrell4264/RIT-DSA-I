6caa8f4e09d16ee1dde75b3200a139af5b871218
nat7611 <nat7611@IGM2350-25>
2018-02-27 17:50:53 -0500

    HW2 doesn't crash, but box2d has some weird physics.

--- GravitySnake/GravitySnake/Snake.cpp
+++ GravitySnake/GravitySnake/Snake.cpp
@@ -3,45 +3,49 @@
 #include "conio.h"
 #include "snake.h"
 //SFML
-#define SFML_STATIC
+/*#define SFML_STATIC
 #include <SFML\Window.hpp>
-#include <SFML\Graphics.hpp>
+#include <SFML\Graphics.hpp>*/
 #pragma endregion
 
-void update() {
-	applyForces();
+float32 timeStep = 1 / 600.0f;
+int32 velocityIterations = 6;
+int32 positionIterations = 2;
+
+void update(b2World& world, b2Body& snake) {
+	applyForces(snake);
 	world.Step(timeStep, velocityIterations, positionIterations);
-	display();
 }
 
-void applyForces() {
-	if (_kbhit != 0) {
-		switch (_getch()) {
-		case('W'):
+void applyForces(b2Body& snake) {
+	if (_kbhit() != 0) {
+		int press = _getch();
+		switch (press) {
+		case('w'):
 		case(38): {
-			snake->ApplyForceToCenter(b2Vec2(0.0f, 1.0f),true);
+			snake.ApplyForceToCenter(b2Vec2(0.0f, 200.0f),true);
 			break;
 		}
-		case('A'):
+		case('a'):
 		case(37): {
-			snake->ApplyForceToCenter(b2Vec2(-1.0f, 0.0f), true);
+			snake.ApplyForceToCenter(b2Vec2(-100.0f, 0.0f), true);
 			break;
 		}
-		case('D'):
+		case('d'):
 		case(39): {
-			snake->ApplyForceToCenter(b2Vec2(1.0f, 0.0f), true);
+			snake.ApplyForceToCenter(b2Vec2(100.0f, 0.0f), true);
 			break;
 		}
 		}
 	}
 }
-void moveTarget(float& xPos, float& yPos) {
-	target->SetTransform(b2Vec2(xPos, yPos), 0);
+void moveTarget(b2Body& target, float& xPos, float& yPos) {
+	target.SetTransform(b2Vec2(xPos, yPos), 0);
 }
 
-void display() {
-	b2Vec2 prPos = snake->GetPosition();
+void display(b2Body& snake, b2Body& target) {
+	b2Vec2 prPos = snake.GetPosition();
 	printf("Snake is at %f, %f\n", prPos.x, prPos.y);
-	prPos = target->GetPosition();
+	prPos = target.GetPosition();
 	printf("Target is at %f, %f\n", prPos.x, prPos.y);
 }
--- GravitySnake/GravitySnake/snake.h
+++ GravitySnake/GravitySnake/snake.h
@@ -2,18 +2,10 @@
 //Includes
 #include "Box2D/box2d.h"
 
-//Variables
-b2Vec2 gravity(0.0f, -9.8f);
-b2World world = b2World(gravity);
-b2Body* snake;
-b2Body* target;
 
-float32 timeStep = 1 / 60.0f;
-int32 velocityIterations = 6;
-int32 positionIterations = 2;
 
 //Functions
-void update();
-void display();
-void applyForces();
-void moveTarget(float& xPos, float& yPos);
+void update(b2World& world, b2Body& snake);
+void display(b2Body& snake, b2Body& target);
+void applyForces(b2Body& snake);
+void moveTarget(b2Body& target, float& xPos, float& yPos);
