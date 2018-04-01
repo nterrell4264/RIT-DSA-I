30ed1e5fc608419bf28fc425a5862a17088a4c1c
Nathan Terrell <nat7611@rit.edu>
2018-03-31 23:08:15 -0400

    PE13

---
+++ PE13 - HandMeDown/HandMeDown/Counter.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "Counter.h"
+#include "windows.h"
+#include <iostream>
+using namespace std;
+
+double PCFreq = 0.0;
+__int64 CounterStart = 0;
+
+void StartCounter()
+{
+	LARGE_INTEGER li;
+	if (!QueryPerformanceFrequency(&li))
+		cout << "QueryPerformanceFrequency failed!\n";
+
+	PCFreq = double(li.QuadPart) / 1000.0;
+
+	QueryPerformanceCounter(&li);
+	CounterStart = li.QuadPart;
+}
+double GetCounter()
+{
+	LARGE_INTEGER li;
+	QueryPerformanceCounter(&li);
+	return double(li.QuadPart - CounterStart) / PCFreq;
+}
